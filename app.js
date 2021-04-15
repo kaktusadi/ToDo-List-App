ed3e7d814f1605bdd036f28a3b98f47a5f3d5b60
Adrian Sienczak <adrian.sienczak@gmail.com>
2021-04-15 11:40:57 +0200

    add .gitignore, procfile and update ports

--- app.js
+++ app.js
@@ -243,6 +243,15 @@ app.get("/about", function(req, res){
   res.render("about");
 });
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000.");
+
+// HEROKU DEPLOYMENT https://devcenter.heroku.com/articles/preparing-a-codebase-for-heroku-deployment
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
+app.listen(port);
+
+
+app.listen(port, function() {
+  console.log("Server has started on port 3000.");
 });
